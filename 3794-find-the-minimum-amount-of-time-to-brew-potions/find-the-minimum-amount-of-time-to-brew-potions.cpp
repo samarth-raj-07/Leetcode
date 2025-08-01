fb13073adc0a1b1707bfcd546f9fcf7da60e075e
Samarth Raj <100614884+samarth-raj-07@users.noreply.github.com>
2025-08-02 00:30:11 +0530

    Time: 175 ms (89.68%) | Memory: 44.6 MB (94.05%) - LeetSync

--- 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.cpp
+++ 3794-find-the-minimum-amount-of-time-to-brew-potions/find-the-minimum-amount-of-time-to-brew-potions.cpp
@@ -4,34 +4,36 @@ struct FastIO {
         cin.tie(NULL);
     }
 } fast_io_init;
+typedef long long ll;
+
 class Solution {
 public:
-    long long minTime(vector<int>& s, vector<int>& mna) {
-        int m = s.size();     
-        int n = mna.size();      
+    ll minTime(vector<int>& s, vector<int>& mna) {
+        ll m = s.size();     
+        ll n = mna.size();      
 
         if (n == 1) {
-            long long total = 0;
+            ll total = 0;
             for (int i = 0; i < m; ++i) {
                 total += 1LL * s[i] * mna[0];
             }
             return total;
         }
 
-        long long currentStart = 0;
-        long long nxt = 0;
+        ll currentStart = 0;
+        ll nxt = 0;
 
         for (int i = 0; i < n - 1; ++i) {
-            long long sumCurr = 0;
-            long long sumNext = 0;
+            ll sumCurr = 0;
+            ll sumNext = 0;
             nxt = currentStart + 1LL * s[0] * mna[i];
 
             for (int j = 1; j < m; ++j) {
                 sumCurr += 1LL * s[j - 1] * mna[i];
                 sumNext += 1LL * s[j - 1] * mna[i + 1];
 
-                long long end= currentStart + sumCurr + 1LL * s[j] * mna[i];
-                long long conflictFix = end- sumNext;
+                ll end= currentStart + sumCurr + 1LL * s[j] * mna[i];
+                ll conflictFix = end- sumNext;
                 nxt = max(nxt, conflictFix);
             }
 
