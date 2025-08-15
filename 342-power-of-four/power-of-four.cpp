d900aca5bb11db85d59fe51d3756930e95269325
Samarth Raj <100614884+samarth-raj-07@users.noreply.github.com>
2025-08-15 20:12:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (4.79%) - LeetSync

---
+++ 342-power-of-four/power-of-four.cpp
@@ -0,0 +1,14 @@
+
+
+class Solution {
+public:
+    bool isPowerOfFour(int n) 
+    {
+        if(n <= 0)
+           return false ;
+
+        double temp = (double) ( log(n))/(log(4) );
+
+        return ceil(temp) == temp ; 
+    }
+};
